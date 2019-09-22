edece830dbd36c223bf83fff7d9b63fc7ec3678d
Simen <Simen.ks@hotmail.com>
2019-09-22 20:40:52 +0200

    Added react helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import Helmet from "react-helmet"
 
 import Layout from "../components/layout"
 import Hero from "../components/Hero"
@@ -8,6 +9,16 @@ import HowToUse from "../components/HowToUse"
 
 const IndexPage = () => (
   <Layout>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>{"Smart City Halden - Haldens billigste bilutleie"}</title>
+      <meta
+        name="description"
+        content={
+          "Prøv ut Haldens billigste leietilbud for bil! Få tilgang til over 20 biler. Alt samlet på en app!"
+        }
+      />
+    </Helmet>
     <Hero />
     <Anmeldelser />
     <HowToUse />
